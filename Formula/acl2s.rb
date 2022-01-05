5eecd48ce38d4ac00a4878d4102d4d85d3e72dcb
Andrew Walter <mist3rwalter@gmail.com>
2022-01-05 17:51:43 -0500

    Fix some formatting issues with the ACL2s formula

--- Formula/acl2s.rb
+++ Formula/acl2s.rb
@@ -1,14 +1,14 @@
 class Acl2s < Formula
-  desc "The ACL2 Sedan theorem prover, built on top of ACL2."
+  desc "ACL2 Sedan theorem prover, built on top of ACL2"
   homepage "http://acl2s.ccs.neu.edu"
-  url "https://api.github.com/repos/acl2/acl2/zipball/b9b73e6b6d0fa76bbc76c9ea25e36e9b26f3d02d"
+  url "https://github.com/acl2/acl2/archive/b9b73e6b6d0fa76bbc76c9ea25e36e9b26f3d02d.tar.gz"
   version "0.1.4"
-  sha256 "537986c4558922051d43e1f4e879ccc94a4b8a5be068d25854dfe3717708b232"
+  sha256 "54e73243b9da162226b718fa92ae9aa99bd8885c672ff1787d907806c0a3715d"
   license "BSD-3-Clause"
   depends_on "sbcl" => :build
 
   resource "sbcl_files" do
-    url "http://prdownloads.sourceforge.net/sbcl/sbcl-2.1.11-source.tar.bz2?download"
+    url "https://sourceforge.net/projects/sbcl/files/sbcl/2.1.11/sbcl-2.1.11-source.tar.bz2"
     sha256 "bfc1481de7fdbdfaeef2ab0f0e8e84efd343433dea8d21cfbea8b0146cbdfefd"
   end
 
@@ -26,10 +26,8 @@ class Acl2s < Formula
     # SBCL install
     rm_rf sbcl_prefix
     mkdir_p sbcl_prefix
-    resource("sbcl_files").stage {
-      if OS.mac?
-        ENV["SBCL_MACOSX_VERSION_MIN"] = MacOS.version
-      end
+    resource("sbcl_files").stage do
+      ENV["SBCL_MACOSX_VERSION_MIN"] = MacOS.version if OS.mac?
       args = [
         "--xc-host=#{HOMEBREW_PREFIX}/bin/sbcl",
         "--prefix=#{sbcl_prefix}",
@@ -37,18 +35,18 @@ class Acl2s < Formula
         "--without-immobile-code",
         "--without-compact-instance-header",
         "--fancy",
-        "--dynamic-space-size=4Gb"
+        "--dynamic-space-size=4Gb",
       ]
       system "./make.sh", *args
       ENV["INSTALL_ROOT"] = sbcl_prefix
       system "sh", "install.sh"
-    }
+    end
 
     # ACL2
     rm_rf scripts_prefix
     rm_rf acl2_prefix
     scripts_prefix.install resource("acl2s_scripts")
-    acl2_prefix.install Dir['*']
+    acl2_prefix.install Dir["*"]
     mkdir_p bin
     ENV.prepend_path "PATH", bin
     # For some reason Homebrew requires that the source
@@ -58,11 +56,11 @@ class Acl2s < Formula
     ln_sf acl2_prefix/"saved_acl2", bin/"acl2"
     ln_sf acl2_prefix/"books/build/cert.pl", bin/"cert.pl"
     ln_sf acl2_prefix/"books/build/clean.pl", bin/"clean.pl"
-    ENV['ACL2S_SCRIPTS'] = scripts_prefix
-    ENV['ACL2_SYSTEM_BOOKS'] = acl2_prefix/"books"
-    ENV['ACL2_LISP'] = sbcl_prefix/"bin/sbcl"
-    ENV['ACL2S_NUM_JOBS'] = "4"
-    ENV['ACL2_SNAPSHOT_INFO'] = 'NONE'
+    ENV["ACL2S_SCRIPTS"] = scripts_prefix
+    ENV["ACL2_SYSTEM_BOOKS"] = acl2_prefix/"books"
+    ENV["ACL2_LISP"] = sbcl_prefix/"bin/sbcl"
+    ENV["ACL2S_NUM_JOBS"] = "4"
+    ENV["ACL2_SNAPSHOT_INFO"] = "NONE"
     cd base_prefix do
       system scripts_prefix/"clean-gen-acl2-acl2s.sh", "--no-git"
     end
