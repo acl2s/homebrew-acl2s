b1594608399f936a0a900dbcbe7df1c248242df1
Andrew Walter <mist3rwalter@gmail.com>
2022-01-24 15:05:14 -0500

    Readd Monterey bottle

--- Formula/acl2s.rb
+++ Formula/acl2s.rb
@@ -9,8 +9,9 @@ class Acl2s < Formula
   bottle do
     root_url "https://github.com/mister-walter/homebrew-acl2s/releases/download/acl2s-0.1.5"
     rebuild 1
-    sha256 catalina:     "70f2a9578c79289fe3381f836a32572811d8f104e6ce9c8552171f254732696e"
-    sha256 x86_64_linux: "30a75d0404332584a0002e292fc260760702c35cc5cbf2dfa3eb5e4ed78b3058"
+    sha256 arm64_monterey: "36993989b59c80c675a60a6130d45bab5137caf308def3a08d383671437b0bc9"
+    sha256 catalina:       "70f2a9578c79289fe3381f836a32572811d8f104e6ce9c8552171f254732696e"
+    sha256 x86_64_linux:   "30a75d0404332584a0002e292fc260760702c35cc5cbf2dfa3eb5e4ed78b3058"
   end
 
   depends_on "sbcl" => :build
