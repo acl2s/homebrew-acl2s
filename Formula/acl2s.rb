f073306708298a140dff3e523d0b2c97ee81c7fc
Andrew Walter <mist3rwalter@gmail.com>
2022-09-12 15:56:58 -0400

    Try SBCL 2.2.6 instead of 2.2.8

--- Formula/acl2s.rb
+++ Formula/acl2s.rb
@@ -10,8 +10,8 @@ class Acl2s < Formula
   depends_on "zlib" unless OS.mac?
 
   resource "sbcl_files" do
-    url "https://downloads.sourceforge.net/project/sbcl/sbcl/2.2.8/sbcl-2.2.8-source.tar.bz2"
-    sha256 "992fcc2b5319010f7587cdc2294b088a595d6c0982ff195b565adfaf4b7d7b0e"
+    url "https://downloads.sourceforge.net/project/sbcl/sbcl/2.2.6/sbcl-2.2.6-source.tar.bz2"
+    sha256 "3e23048c8fa826fb913220beb2ac3697dbc5c0cdf2e89fed8db39ed1712304a0"
   end
 
   resource "acl2s_scripts" do
