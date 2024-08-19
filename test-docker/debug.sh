4e4a411e094b05a5ffd310a84584eb423977536c
Drew Walter <mist3rwalter@gmail.com>
2024-08-19 17:00:40 -0400

    Fix shell script files once more

--- test-docker/debug.sh
+++ test-docker/debug.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PARENT_DIR="$( dirname "${SCRIPT_DIR}" )"
+#!/usr/bin/env bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PARENT_DIR="$(dirname "${SCRIPT_DIR}")"
 cd "${PARENT_DIR}" || exit 1
 
 # This script uses fancy experimental Docker BuildKit features to
